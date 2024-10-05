4a8346d96371f7dc65e09ffe2c874de586b7bee9
mikasenghaas <mail@mikasenghaas.de>
2024-10-05 12:02:49 +0000

    Added remote setup script

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # SWARM
 
 This repository is a re-implementation and benchmark of [SWARM](https://arxiv.org/abs/2301.11913) parallelism. 
+
+## Remote Setup
+
+```bash
+PERSISTENT_DIR=/workspace
+git clone https://github.com/mikasenghaas/swarm.git $PERSISTENT_DIR/swarm
+scp .env <user>@<host>:$PERSISTENT_DIR/swarm/.env
+bash $PERSISTENT_DIR/swarm/scripts/setup.sh
+```
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
+# Load .env
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
 # Install miniconda
 set -e
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O install_miniconda.sh
-bash install_miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-
-# Clone repository
-git clone https://github.com/mikasenghaas/swarm.git
-cd swarm
+bash install_miniconda.sh -b -p $PWD/miniconda
+export PATH="$PWD/miniconda/bin:$PATH"
+conda init
+source ~/.bashrc
 
 # Create conda environment
-conda create -n swarm python~=3.10.15 pip
+conda create -n swarm python~=3.10.15 -y
 conda activate swarm
 
 # Install additional dependencies
-pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+cd swarm
+pip install --user --upgrade pip
+pip install --user --no-cache-dir -r requirements.txt
+
+# Setup git
+echo "Setting up git"
+git config --global user.name "mikasenghaas"
+git config --global user.email "mikasenghaas@gmail.com"
+git config --global credential.helper store
+echo "https://mikasenghaas:${GIT_TOKEN}@github.com" > ~/.git-credentials
+
+# Setup keys
+echo "Setting up W&B API key"
+python -m wandb login $WANDB_TOKEN
+
+# Setup W&B
+echo "Setting up HF login"
+huggingface-cli login --token $HF_TOKEN --add-to-git-credential
