faf057ab4dbb94de3dd6870fe50d3526e0f72e55
Mika Senghaas <mail@mikasenghaas.de>
2024-11-12 18:21:53 +0000

    Add overfit and checkpoint experiments

---
+++ experiments/verify/baseline/overfit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+GROUP="verify/baseline/overfit"
+
+# Single-GPU Experiments to overfit a single sentence
+# Run 1: LLama 2 (9M)
+# Run 2: GPT-2 (124M)
+
+MODELS="llama2-9m gpt2-124m"
+for MODEL in $MODELS; do
+    python src/train/baseline.py @configs/debug.toml \
+        --model @configs/model/$MODEL.toml \
+        --data @configs/data/memorize.toml \
+        --sample.enable true \
+        --train.max_steps 100 \
+        --train.batch_size 1 \
+        --train.micro_batch_size 1 \
+        --logging.wandb.enable true \
+        --logging.wandb.group $GROUP
+done
