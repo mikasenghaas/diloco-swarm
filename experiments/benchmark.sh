ef903044357d97b40e48e15f98b8b31837b48cca
Mika Senghaas <mail@mikasenghaas.de>
2024-12-17 18:22:02 +0000

    Simplify experiment configs

--- experiments/benchmark.sh
+++ experiments/benchmark.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 TAGS="Benchmark"
 
 # Benchmarking GPU throughput of GPT-2 on RTX 3090
@@ -11,54 +10,102 @@ TAGS="Benchmark"
 # Sequence Length = 1024
 # => 512 * 1024 = 524288 ~ 0.5M T/step
 
-# Micro Batch Size: [1, 2, 4, 8, 16]
+# Constants
+SEQ_LEN=1024
+BATCH_SIZE=512
+MAX_STEPS=5
+
+# Vary
 MICRO_BATCH_SIZES=(1 2 4 8 16)
+MAX_MICRO_BATCHES=(1 2 4 8 16 32 64 128)
 
 # Single GPU
-for MICRO_BATCH_SIZE in "${MICRO_BATCH_SIZES[@]}"
-do
-    torchrun --nproc_per_node 1 src/train.py \
-        @configs/benchmark.toml \
-        --swarm.num_stages 1 \
-        --model @configs/model/gpt2-small.toml \
-        --data @configs/data/wikitext.toml \
-        --train.micro_batch_size $MICRO_BATCH_SIZE \
-        --logging.wandb.tags $TAGS
-done
-
-# DP
 # for MICRO_BATCH_SIZE in "${MICRO_BATCH_SIZES[@]}"
 # do
-#     torchrun --nproc_per_node 2 src/train.py \
-#         @configs/benchmark.toml \
+#     torchrun --nproc_per_node 1 src/train.py \
 #         --swarm.num_stages 1 \
 #         --model @configs/model/gpt2-small.toml \
 #         --data @configs/data/wikitext.toml \
+#         --train.batch_size $BATCH_SIZE \
+#         --data.seq_length $SEQ_LEN \
+#         --train.max_steps $MAX_STEPS \
 #         --train.micro_batch_size $MICRO_BATCH_SIZE \
-#         --logging.wandb.tags $TAGS
+#         --eval.enable false \
+#         --sample.enable false \
+#         --logging.wandb.enable true \
+#         --logging.wandb.tags "$TAGS,Single-GPU"
 # done
-# 
-# # PP
+
+# DP (2 Workers)
 # for MICRO_BATCH_SIZE in "${MICRO_BATCH_SIZES[@]}"
 # do
 #     torchrun --nproc_per_node 2 src/train.py \
-#         @configs/benchmark.toml \
-#         --swarm.num_stages 2 \
+#         --swarm.num_stages 1 \
 #         --model @configs/model/gpt2-small.toml \
 #         --data @configs/data/wikitext.toml \
+#         --train.batch_size $BATCH_SIZE \
+#         --data.seq_length $SEQ_LEN \
+#         --train.max_steps $MAX_STEPS \
 #         --train.micro_batch_size $MICRO_BATCH_SIZE \
-#         --logging.wandb.tags $TAGS
+#         --eval.enable false \
+#         --sample.enable false \
+#         --logging.wandb.enable true \
+#         --logging.wandb.tags "$TAGS,DP"
 # done
-# 
-# # SWARM
+
+# DP (4 Workers)
 # for MICRO_BATCH_SIZE in "${MICRO_BATCH_SIZES[@]}"
 # do
 #     torchrun --nproc_per_node 4 src/train.py \
-#         @configs/benchmark.toml \
-#         --world.num_stages 2 \
+#         --swarm.num_stages 1 \
 #         --model @configs/model/gpt2-small.toml \
 #         --data @configs/data/wikitext.toml \
+#         --train.batch_size $BATCH_SIZE \
+#         --data.seq_length $SEQ_LEN \
+#         --train.max_steps $MAX_STEPS \
 #         --train.micro_batch_size $MICRO_BATCH_SIZE \
-#         --logging.wandb.tags $TAGS
+#         --eval.enable false \
+#         --sample.enable false \
+#         --logging.wandb.enable true \
+#         --logging.wandb.tags "$TAGS,DP"
 # done
-# 
+
+# DP (8 Workers)
+# for MICRO_BATCH_SIZE in "${MICRO_BATCH_SIZES[@]}"
+# do
+#     torchrun --nproc_per_node 8 src/train.py \
+#         --swarm.num_stages 1 \
+#         --model @configs/model/gpt2-small.toml \
+#         --data @configs/data/wikitext.toml \
+#         --train.batch_size $BATCH_SIZE \
+#         --data.seq_length $SEQ_LEN \
+#         --train.max_steps $MAX_STEPS \
+#         --train.micro_batch_size $MICRO_BATCH_SIZE \
+#         --eval.enable false \
+#         --sample.enable false \
+#         --logging.wandb.enable true \
+#         --logging.wandb.tags "$TAGS,DP"
+# done
+
+# PP (2 Workers)
+for MICRO_BATCH_SIZE in "${MICRO_BATCH_SIZES[@]}"
+do
+    for MAX_MICRO_BATCHES in "${MAX_MICRO_BATCHES[@]}"
+    do
+        if [ $(($MICRO_BATCH_SIZE * $MAX_MICRO_BATCHES)) -le 128 ]; then
+            torchrun --nproc_per_node 2 src/train.py \
+                --swarm.num_stages 2 \
+                --model @configs/model/gpt2-small.toml \
+                --data @configs/data/wikitext.toml \
+                --train.batch_size $BATCH_SIZE \
+                --data.seq_length $SEQ_LEN \
+                --train.max_steps $MAX_STEPS \
+                --train.micro_batch_size $MICRO_BATCH_SIZE \
+                --train.max_micro_batches $MAX_MICRO_BATCHES \
+                --eval.enable false \
+                --sample.enable false \
+                --logging.wandb.enable true \
+                --logging.wandb.tags "$TAGS,PP"
+        fi
+    done
+done
--- experiments/memorize.sh
+++ experiments/memorize.sh
@@ -3,34 +3,61 @@
 set -e
 TAGS="Memorize"
 
+SEQ_LEN=128
+SUBSET_SIZE=1
+MAX_STEPS=100
+BATCH_SIZE=1
+MICRO_BATCH_SIZE=1
+ENABLE_WANDB=true
+
 # Single GPU
 torchrun --nproc_per_node 1 src/train.py \
-    @configs/memorize.toml \
     --swarm.num_stages 1 \
     --model @configs/model/gpt2-small.toml \
     --data @configs/data/memorize.toml \
+    --train.batch_size $BATCH_SIZE \
+    --data.seq_length $SEQ_LEN \
+    --train.max_steps $MAX_STEPS \
+    --train.micro_batch_size $MICRO_BATCH_SIZE \
+    --eval.enable false \
+    --logging.wandb.enable $ENABLE_WANDB \
     --logging.wandb.tags "$TAGS,Single-GPU"
 
 # DP
-torchrun --nproc_per_node 2 src/train.py \
-    @configs/memorize.toml \
+torchrun --nproc_per_node 4 src/train.py \
     --swarm.num_stages 1 \
     --model @configs/model/gpt2-small.toml \
     --data @configs/data/memorize.toml \
+    --train.batch_size $BATCH_SIZE \
+    --data.seq_length $SEQ_LEN \
+    --train.max_steps $MAX_STEPS \
+    --train.micro_batch_size $MICRO_BATCH_SIZE \
+    --eval.enable false \
+    --logging.wandb.enable $ENABLE_WANDB \
     --logging.wandb.tags "$TAGS,DP"
 
 # PP
-torchrun --nproc_per_node 2 src/train.py \
-    @configs/memorize.toml \
-    --swarm.num_stages 2 \
+torchrun --nproc_per_node 4 src/train.py \
+    --swarm.num_stages 4 \
     --model @configs/model/gpt2-small.toml \
     --data @configs/data/memorize.toml \
+    --train.batch_size $BATCH_SIZE \
+    --data.seq_length $SEQ_LEN \
+    --train.max_steps $MAX_STEPS \
+    --train.micro_batch_size $MICRO_BATCH_SIZE \
+    --eval.enable false \
+    --logging.wandb.enable $ENABLE_WANDB \
     --logging.wandb.tags "$TAGS,PP"
 
 # SWARM
 torchrun --nproc_per_node 4 src/train.py \
-    @configs/memorize.toml \
     --swarm.num_stages 2 \
     --model @configs/model/gpt2-small.toml \
     --data @configs/data/memorize.toml \
+    --train.batch_size $BATCH_SIZE \
+    --data.seq_length $SEQ_LEN \
+    --train.max_steps $MAX_STEPS \
+    --train.micro_batch_size $MICRO_BATCH_SIZE \
+    --eval.enable false \
+    --logging.wandb.enable $ENABLE_WANDB \
     --logging.wandb.tags "$TAGS,SWARM"
