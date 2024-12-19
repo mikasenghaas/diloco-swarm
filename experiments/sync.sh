4436eed6ef15e21a5d34d06d88c0a2f25b3ebdbc
Mika Senghaas <mail@mikasenghaas.de>
2024-12-19 12:21:17 +0000

    Update sync experiments

--- experiments/sync.sh
+++ experiments/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-TAGS="Test,Sync"
+TAGS="Sync"
 
 # Baseline (Single GPU)
 # torchrun --nproc_per_node 1 src/train.py \
@@ -19,7 +19,7 @@ TAGS="Test,Sync"
 #     --logging.wandb.tags "$TAGS,Single-GPU"
 
 # Baseline (DP)
-SYNC_EVERY_N_STEPS=(100)
+SYNC_EVERY_N_STEPS=(1)
 for SYNC_EVERY_N_STEPS in ${SYNC_EVERY_N_STEPS[@]}; do
     torchrun --nproc_per_node 2 src/train.py \
         --swarm.num_stages 1 \
@@ -28,11 +28,12 @@ for SYNC_EVERY_N_STEPS in ${SYNC_EVERY_N_STEPS[@]}; do
         --data @configs/data/fineweb-edu-1bt.toml \
         --data.subset_size 0.1 \
         --train.micro_batch_size 16 \
-        --train.step_timeout 60 \
+        --train.step_timeout 30 \
+        --train.scheduler.enable true \
         --eval.every_n_steps 10 \
         --eval.max_steps 10 \
         --sample.enable true \
-        --sample.every_n_steps 10 \
+        --sample.every_n_steps 50 \
         --logging.wandb.enable true \
         --logging.wandb.tags "$TAGS,DP"
 done
