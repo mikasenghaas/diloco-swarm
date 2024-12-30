e4019148b362ef0c173769267f7e1d97440e9bf5
Mika Senghaas <mail@mikasenghaas.de>
2024-12-30 11:13:05 +0000

    Add DiLoCo experiment

--- experiments/main.sh
+++ experiments/main.sh
@@ -3,7 +3,7 @@
 set -e
 TAGS="Main"
 
-# Baseline (Single GPU)
+# Baseline (Single GPU) - DONE
 # torchrun --nproc_per_node 1 src/train.py \
 #     --swarm.num_stages 1 \
 #     --swarm.sync_every_n_steps 1 \
@@ -19,22 +19,69 @@ TAGS="Main"
 #     --data.seq_length 1024 \
 #     --train.micro_batch_size 64 \
 #     --eval.enable true \
-#     --eval.every_n_steps 20 \
+#     --eval.every_n_steps 50 \
 #     --eval.eval_size 0.001 \
 #     --sample.enable true \
 #     --sample.every_n_steps 100 \
 #     --logging.wandb.enable true \
 #     --logging.wandb.tags "$TAGS,Single-GPU"
 
-# Baseline (DP, 8xBatch Size)
+# Baseline (DP, 8xBatch Size) - DONE
+# torchrun --nproc_per_node 8 src/train.py \
+#     --swarm.num_stages 1 \
+#     --swarm.sync_every_n_steps 1 \
+#     --model @configs/model/gpt2-small.toml \
+#     --data @configs/data/fineweb-edu-10bt.toml \
+#     --train.inner_optimizer @configs/optimizer/adamw.toml \
+#     --train.outer_optimizer @configs/optimizer/none.toml \
+#     --train.inner_optimizer.lr 4e-4 \
+#     --train.scheduler.enable true \
+#     --train.scheduler.num_warmup_steps 50 \
+#     --train.max_steps 2000 \
+#     --train.batch_size 4096 \
+#     --data.seq_length 1024 \
+#     --train.micro_batch_size 64 \
+#     --eval.enable true \
+#     --eval.every_n_steps 50 \
+#     --eval.eval_size 0.001 \
+#     --sample.enable true \
+#     --sample.every_n_steps 100 \
+#     --logging.wandb.enable true \
+#     --logging.wandb.tags "$TAGS,DP,8xBatchSize"
+
+# Baseline (DP, 8xUpdates)
+# torchrun --nproc_per_node 8 src/train.py \
+#     --swarm.num_stages 1 \
+#     --swarm.sync_every_n_steps 1 \
+#     --model @configs/model/gpt2-small.toml \
+#     --data @configs/data/fineweb-edu-10bt.toml \
+#     --train.inner_optimizer @configs/optimizer/adamw.toml \
+#     --train.outer_optimizer @configs/optimizer/none.toml \
+#     --train.inner_optimizer.lr 4e-4 \
+#     --train.scheduler.enable true \
+#     --train.scheduler.num_warmup_steps 50 \
+#     --train.max_steps 16000 \
+#     --train.batch_size 512 \
+#     --data.seq_length 1024 \
+#     --train.micro_batch_size 64 \
+#     --eval.enable true \
+#     --eval.every_n_steps 50 \
+#     --eval.eval_size 0.001 \
+#     --sample.enable true \
+#     --sample.every_n_steps 100 \
+#     --logging.wandb.enable true \
+#     --logging.wandb.tags "$TAGS,DP,8xUpdates"
+
+# Baseline (DiLoCo, 8xBatch Size)
 torchrun --nproc_per_node 8 src/train.py \
     --swarm.num_stages 1 \
-    --swarm.sync_every_n_steps 1 \
+    --swarm.sync_every_n_steps 50 \
     --model @configs/model/gpt2-small.toml \
     --data @configs/data/fineweb-edu-10bt.toml \
     --train.inner_optimizer @configs/optimizer/adamw.toml \
-    --train.outer_optimizer @configs/optimizer/none.toml \
+    --train.outer_optimizer @configs/optimizer/nesterov.toml \
     --train.inner_optimizer.lr 4e-4 \
+    --train.outer_optimizer.lr 0.7 \
     --train.scheduler.enable true \
     --train.scheduler.num_warmup_steps 50 \
     --train.max_steps 2000 \
@@ -42,32 +89,9 @@ torchrun --nproc_per_node 8 src/train.py \
     --data.seq_length 1024 \
     --train.micro_batch_size 64 \
     --eval.enable true \
-    --eval.every_n_steps 20 \
+    --eval.every_n_steps 50 \
     --eval.eval_size 0.001 \
     --sample.enable true \
     --sample.every_n_steps 100 \
     --logging.wandb.enable true \
-    --logging.wandb.tags "$TAGS,DP,8xBatchSize"
-
-# Baseline (DP, 8xUpdates)
-torchrun --nproc_per_node 8 src/train.py \
-    --swarm.num_stages 1 \
-    --swarm.sync_every_n_steps 1 \
-    --model @configs/model/gpt2-small.toml \
-    --data @configs/data/fineweb-edu-10bt.toml \
-    --train.inner_optimizer @configs/optimizer/adamw.toml \
-    --train.outer_optimizer @configs/optimizer/none.toml \
-    --train.inner_optimizer.lr 4e-4 \
-    --train.scheduler.enable true \
-    --train.scheduler.num_warmup_steps 50 \
-    --train.max_steps 8000 \
-    --train.batch_size 512 \
-    --data.seq_length 1024 \
-    --train.micro_batch_size 64 \
-    --eval.enable true \
-    --eval.every_n_steps 20 \
-    --eval.eval_size 0.001 \
-    --sample.enable true \
-    --sample.every_n_steps 100 \
-    --logging.wandb.enable true \
-    --logging.wandb.tags "$TAGS,DP,8xUpdates"
+    --logging.wandb.tags "$TAGS,DiLoCo"
--- experiments/test.sh
+++ experiments/test.sh
@@ -3,26 +3,27 @@
 set -e
 
 TAGS="Test,Main"
-CMD="torchrun --nproc_per_node 1 src/train.py \
+CMD="torchrun --nproc_per_node 8 src/train.py \
     --swarm.num_stages 1 \
-    --swarm.sync_every_n_steps 1 \
+    --swarm.sync_every_n_steps 50 \
     --model @configs/model/gpt2-small.toml \
     --data @configs/data/fineweb-edu-10bt.toml \
     --train.inner_optimizer @configs/optimizer/adamw.toml \
-    --train.outer_optimizer @configs/optimizer/none.toml \
+    --train.outer_optimizer @configs/optimizer/nesterov.toml \
     --train.inner_optimizer.lr 4e-4 \
+    --train.outer_optimizer.lr 0.7 \
     --train.scheduler.enable true \
     --train.scheduler.num_warmup_steps 50 \
     --train.max_steps 2000 \
-    --train.batch_size 512 \
+    --train.batch_size 4096 \
     --data.seq_length 1024 \
     --train.micro_batch_size 64 \
     --eval.enable true \
-    --eval.every_n_steps 10 \
+    --eval.every_n_steps 50 \
     --eval.eval_size 0.001 \
     --sample.enable true \
-    --sample.every_n_steps 50 \
+    --sample.every_n_steps 100 \
     --logging.wandb.enable false \
-    --logging.wandb.tags '$TAGS,Single-GPU'"
+    --logging.wandb.tags '$TAGS,DiLoCo'"
 
 echo $CMD; eval $CMD;
